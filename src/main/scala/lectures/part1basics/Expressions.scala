3cbdcea828a67a596ed70664c83ba86e77893290
saitarndgc <saitarn.d@pttgcgroup.com>
2024-11-28 16:44:35 +0700

    expressions

---
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -0,0 +1,18 @@
+package lectures.part1basics
+
+/**
+ * Created by saita on 28-11-2024.
+ */
+object Expressions extends App{
+	val x :Int = 1 + 2
+
+	println(2 * 4 + 1)
+
+	println(1 == x)
+	println(!(1 == x))
+	var aVariable = 2
+
+	println(aVariable)
+	aVariable += 3
+	println(aVariable)
+}
