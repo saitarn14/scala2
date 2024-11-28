99ee5316b24825e0f41f5b2f793c3effdd269562
saitarndgc <saitarn.d@pttgcgroup.com>
2024-11-29 01:51:16 +0700

    expressions

--- src/main/scala/lectures/part1basics/Expressions.scala
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -15,4 +15,43 @@ object Expressions extends App{
 	println(aVariable)
 	aVariable += 3
 	println(aVariable)
+
+	// IF Expression
+	private val aCondition = true
+	private val aConditionedValue = if (aCondition) 9 else 7
+	println(aConditionedValue)
+	println(if(aCondition) 9 else 7)
+
+
+	//LOOP
+	println("LOOP\n")
+	var i = 0
+	while (i < 10) {
+		println(i)
+		i += 1
+	}
+
+	val aWeirdValue = (aVariable = 3) //Unit === vode
+	println(aWeirdValue)
+
+	val aCodeBlock: String = {
+		val y: Int = 2
+		val z: Int = y + 1
+
+		if (z > 2) "hello" else "goodbye"
+	}
+
+//	val anotherValue = z + 1
+	val someValue = {
+		2 < 3
+	}
+	println(someValue)
+
+	val someOtherValue = {
+		if(someValue) 234 else 998
+		88
+
+	}
+	println(someOtherValue)
 }
+
