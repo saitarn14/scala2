a141c80fbaee28cecdb46a5fe1cf52f78fc9ffa4
saitarndgc <saitarn.d@pttgcgroup.com>
2024-12-04 22:01:12 +0700

    call by name - value

---
+++ src/main/scala/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,31 @@
+package lectures.part1basics
+
+/**
+ * Created by saita on 01-12-2024.
+ */
+object CBNvsCBV extends App{
+	def callByValue(x: Long): Unit = {
+		println("by value: " + x)
+		println("by value: " + x)
+
+		// CALL BY VALUE = Sent value to function
+	}
+
+	def callByName(x: => Long): Unit = {
+		println("by name: " + x)
+		println("by name: " + x)
+		// CALL BY NAME = Sent function[System.nanoTime()] to function [callByName()]
+	}
+
+	callByValue(System.nanoTime())
+//	callByValue(545627786539500L)
+
+	callByName(System.nanoTime())
+//	callByName(System.nanoTime())
+
+	def infinite(): Int = 1 + infinite()
+	def printFirst(x: Int, y: => Int) = println(x)
+
+//	printFirst(infinite(), 24)
+	printFirst(24, infinite())
+}
