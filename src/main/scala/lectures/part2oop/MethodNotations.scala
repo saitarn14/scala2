4df8b215ee6149cc17b3e27fa6ac85590fe473ce
saitarndgc <saitarn.d@pttgcgroup.com>
2024-12-05 23:21:33 +0700

    Method Notation exercises

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -7,7 +7,7 @@ import scala.language.postfixOps
  */
 object MethodNotations extends App {
 	println("test")
-	class Person(val name: String, favoriteMovie: String) {
+	class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
 		def likes(movie: String): Boolean = {
 			movie == favoriteMovie
 		}
@@ -16,8 +16,14 @@ object MethodNotations extends App {
 		def +(person: Person): String = s"${this.name} is hanging out plus with ${person.name}"
 		def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
 		def unary_! : String = s"$name What 's up"
+		def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
 		def isAlive: Boolean = true
 		def apply(): String = s"Hi my apply, my name is $name and I like $favoriteMovie"
+		def apply(n: Int): String = s"$name Watched $favoriteMovie $n times"
+
+		def learns(thing: String):String = s"$name is learning $thing"
+//		def learnsScala: String = this.learns("Scala")
+		def learnsScala = this learns "Scala"
 
 	}
 
@@ -71,4 +77,10 @@ object MethodNotations extends App {
 	 */
 	println((fonfon + "The rock star")())
 	println((fonfon + "The rock star").apply())
+
+	println((+fonfon).age)
+	println(fonfon learnsScala)
+
+	println(fonfon.apply(10))
+	println(fonfon(11))
 }
